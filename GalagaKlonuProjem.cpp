95b35efaeadb817d304874becb2835b03c4224d5
Elif <gelenelif1@gmail.com>
2026-05-26 17:16:11 +0300

    5. commit:Düşman sınıfı oluşturuldu.

--- GalagaKlonuProjem.cpp
+++ GalagaKlonuProjem.cpp
@@ -29,3 +29,58 @@ public:
         sekil.move(0, hiz);
     }
 };
+// Dusman Durumlari (Dalis mekanizmasi icin)
+enum class DusmanDurumu { Formasyon, Dalista };
+
+// Dusman Sinifi
+class Dusman {
+public:
+    sf::ConvexShape sekil;
+    sf::Vector2f formasyonPozisyonu;
+    sf::Vector2f aktifPozisyonu;
+    DusmanDurumu durum;
+    float dalisHizi;
+    float sinusZamani;
+
+    Dusman(float x, float y) {
+        formasyonPozisyonu = sf::Vector2f(x, y);
+        aktifPozisyonu = formasyonPozisyonu;
+        durum = DusmanDurumu::Formasyon;
+        dalisHizi = 3.5f;
+        sinusZamani = static_cast<float>(std::rand() % 100);
+
+        // Klasik Galaga tarzi ucgen/bocek gorunumu olusturma
+        sekil.setPointCount(5);
+        sekil.setPoint(0, sf::Vector2f(15, 0));
+        sekil.setPoint(1, sf::Vector2f(30, 20));
+        sekil.setPoint(2, sf::Vector2f(22, 30));
+        sekil.setPoint(3, sf::Vector2f(8, 30));
+        sekil.setPoint(4, sf::Vector2f(0, 20));
+
+        sekil.setFillColor(sf::Color::Green);
+        sekil.setOrigin(15, 15);
+        sekil.setPosition(aktifPozisyonu);
+    }
+
+    void guncelle(float formasyonOfsetX) {
+        if (durum == DusmanDurumu::Formasyon) {
+            // Sinuzoidal formasyon hareketi (saga ve sola ortak salinim)
+            aktifPozisyonu.x = formasyonPozisyonu.x + formasyonOfsetX;
+            sekil.setPosition(aktifPozisyonu);
+        }
+        else if (durum == DusmanDurumu::Dalista) {
+            // Dalisa gecen dusman asagi inerken hafifce yalpalar
+            aktifPozisyonu.y += dalisHizi;
+            sinusZamani += 0.05f;
+            aktifPozisyonu.x += std::sin(sinusZamani) * 2.0f;
+            sekil.setPosition(aktifPozisyonu);
+
+            // Ekranin altindan cikan dusman tekrar uste, formasyona doner
+            if (aktifPozisyonu.y > EKRAN_YUKSEKLIK + 20) {
+                aktifPozisyonu.y = formasyonPozisyonu.y;
+                durum = DusmanDurumu::Formasyon;
+            }
+        }
+    }
+};
+
