81feca25747254a992a38a667d835de54bb6c8e4
Elif <gelenelif1@gmail.com>
2026-05-26 11:19:53 +0300

    3. commit:Kütüphane eklendi ve oyun ekranı oluşturuldu.

--- GalagaKlonuProjem.cpp
+++ GalagaKlonuProjem.cpp
@@ -1,20 +1,10 @@
-﻿// GalagaKlonuProjem.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
-//
+﻿#include <SFML/Graphics.hpp>
+#include <vector>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <string>
 
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
-}
-
-// Programı çalıştır: Ctrl + F5 veya Hata Ayıkla > Hata Ayıklamadan Başlat menüsü
-// Programda hata ayıkla: F5 veya Hata Ayıkla > Hata Ayıklamayı Başlat menüsü
-
-// Kullanmaya Başlama İpuçları: 
-//   1. Dosyaları eklemek/yönetmek için Çözüm Gezgini penceresini kullanın
-//   2. Kaynak denetimine bağlanmak için Takım Gezgini penceresini kullanın
-//   3. Derleme çıktısını ve diğer iletileri görmek için Çıktı penceresini kullanın
-//   4. Hataları görüntülemek için Hata Listesi penceresini kullanın
-//   5. Yeni kod dosyaları oluşturmak için Projeye Git > Yeni Öğe ekle veya varolan kod dosyalarını projeye eklemek için Proje > Var Olan Öğeyi Ekle adımlarını izleyin
-//   6. Bu projeyi daha sonra yeniden açmak için Dosya > Aç > Proje'ye gidip .sln uzantılı dosyayı seçin
+// --- OYUN AYARLARI ---
+const int EKRAN_GENISLIK = 600;
+const int EKRAN_YUKSEKLIK = 800;
