2955ecef35adfc0bf4acc87e8446c5efe84c8df9
Elif <gelenelif1@gmail.com>
2026-05-26 11:27:00 +0300

    4. commit:Mermi sınıfı oluşturuldu.

--- GalagaKlonuProjem.cpp
+++ GalagaKlonuProjem.cpp
@@ -8,3 +8,24 @@
 // --- OYUN AYARLARI ---
 const int EKRAN_GENISLIK = 600;
 const int EKRAN_YUKSEKLIK = 800;
+
+// Mermi Sinifi
+class Mermi {
+public:
+    sf::RectangleShape sekil;
+    float hiz;
+    bool oyuncuMermisi;
+
+    Mermi(float x, float y, bool oyuncununMu) {
+        oyuncuMermisi = oyuncununMu;
+        // Oyuncu mermisi yukari (-), dusman mermisi asagi (+) gider
+        hiz = oyuncuMermisi ? -7.0f : 4.0f;
+        sekil.setSize(sf::Vector2f(4, 12));
+        sekil.setFillColor(oyuncuMermisi ? sf::Color::Blue : sf::Color::Red);
+        sekil.setPosition(x, y);
+    }
+
+    void guncelle() {
+        sekil.move(0, hiz);
+    }
+};
